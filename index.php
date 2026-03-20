03d29f4c89a99b4008243008f0e896c48e752c09
The Noctura <muhamadalghani584@gmail.com>
2026-03-20 14:29:06 +0700

    feat: add beranda page with header, footer, banner, and navbar styling

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Berand | PasarKita</title>
+  <link rel="stylesheet" href="./assets/css/beranda.css">
+</head>
+<body>
+  <?php include 'includes/header.php' ?>
+
+  <!-- Hero Section -->
+  <main>
+    <section class="hero">
+      <div class="container">
+        <h1><span>Sayur & Ayam Segar</span>, Langsung ke Pintu Rumahmu</h1>
+        <p>Tidak perlu kepasar. Pesan sekarang, kami antar hari ini</p>
+        <a href="checkout.php">Pesan Sekarang</a>
+      </div>
+    </section>
+  </main>
+</body>
+</html>
